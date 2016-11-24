5cbdcd15225ed68fcd8b31a3fbd8ce6b9cf28fe2
qorehowl44 <qorehowl44@naver.com>
2016-11-24 09:56:45 +0900

    ExArray class implement, practice Template

--- Git_practice/Main.cpp
+++ Git_practice/Main.cpp
@@ -133,6 +133,7 @@ int main()
 
 	return 0;
 }*/
+/*
 #include"Frame.h"
 void main()
 {
@@ -148,4 +149,48 @@ void main()
 	{
 		cout << "Error msg :" << msg << endl;
 	}
+}*/
+#pragma once
+#include<string>
+#include"Frame.h"
+#include"ExArray.h"
+
+template<typename T>
+void mySwap(T& num1, T& num2)
+{
+	T temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
+void main()
+{
+	ExArray<int> arr1(3);
+	ExArray<char> arr2(4);
+	ExArray<double> arr3(5);
+
+	arr1.addData(2);
+	arr2.addData('A');
+	arr3.addData(32.12);
+
+	arr1.printData();
+	arr2.printData();
+	arr3.printData();
+
+	int num1 = 10, num2 = 40;
+	cout << "before : " << num1 << ", " << num2 << endl;
+	mySwap<int>(num1, num2);
+	cout << "after : " << num1 << ", " << num2 << endl;
+
+	double num1b = 121.11, num2b = 10.5;
+	cout << "before : " << num1b << ", " << num2b << endl;
+	mySwap<double>(num1b, num2b);
+	cout << "after : " << num1b << ", " << num2b << endl;
+
+	ExArray<int> arr4(5);
+	arr4.addData(10);
+
+	mySwap<ExArray<int>>(arr1, arr4);
+	arr1.printData();
+	arr4.printData();
+
 }
